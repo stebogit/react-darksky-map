55ed6a395f0f392bd53f38182a69ef8c182b4802
stebogit <stefano.borghi@gmail.com>
2018-05-30 23:50:30 -0700

    added eslint

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 class DarkskyMap extends React.Component {
-  render() {
-    return (
-    	<div>
-      		<div>Package: react-darksky-map</div>
-      		<div>Component: DarkskyMap</div>
-      	</div>
-    );
-  }
+    render () {
+        return (
+            <div>
+                <div>Package: react-darksky-map</div>
+                <div>Component: DarkskyMap</div>
+            </div>
+        );
+    }
 }
 
 // DarkskyMap.propTypes = {
